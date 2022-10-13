a6d6fa88eeef26264cc8c2a44ca821e08fd4bb9b
Marcela Aristizabal <marcea@Marcelas-MacBook-Pro.local>
2022-10-13 18:19:27 -0500

    Creación front

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tienda Virtual DevMountain</h1>
+        <img src="./images/carsstore.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
